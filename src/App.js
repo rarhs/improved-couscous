0851f66e2b08bc3058baa37bd717bb6b3535bc4b
rarhs <rarhs@users.noreply.github.com>
2023-01-08 07:29:04 +0000

    add event handlers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import LearnReactLink from './LearnReactLink';
 
 function App() {
   const[count, setCount] = useState(0);
+  const[email, setEmail] = useState('');
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Thank you for subscribing with email: ${email}");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -27,6 +37,16 @@ function App() {
         </p>
         <p>You clicked the button {count} times</p>
         <button onClick={()=> setCount(count+1)}>click me</button>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input type="email" value={email} onChange={handleChange} />
+          </label>
+          <button type="submit">
+            Subscribe
+          </button>
+
+        </form>
       </header>
     </div>
   );
