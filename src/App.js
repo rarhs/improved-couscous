575b1461c4af847e5daf545df6752ece63ce79ce
rarhs <rarhs@users.noreply.github.com>
2023-01-08 03:37:52 +0000

    Added Logo and LearnReactLink component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import './App.css';
+import Logo from './Logo';
+import LearnReactLink from './LearnReactLink';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+      <Logo src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
@@ -12,14 +15,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          <a
+          <LearnReactLink
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </LearnReactLink>
         </p>
       </header>
     </div>
---
+++ src/LearnReactLink.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LearnReactLink(props) {
+    return <a className={props.className} href={props.href} target={props.target} >Learn React</a>
+}
+
+export default LearnReactLink;
